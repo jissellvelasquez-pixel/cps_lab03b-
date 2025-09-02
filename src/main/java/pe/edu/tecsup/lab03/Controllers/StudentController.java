570b8116b9e2c6597e92d207f4fb5aa9c123611b
Jissell <jissell.velasquez@tecsup.edu.pe>
2025-09-02 04:13:47 +0200

    Agregar clase StudentController en controllers

--- src/main/java/pe/edu/tecsup/lab03/Controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/Controllers/StudentController.java
@@ -1,4 +1,8 @@
 package pe.edu.tecsup.lab03.Controllers;
 
 public class StudentController {
+
+    public String helloStudent() {
+        return "Hola estudiante, el controlador funciona correctamente!";
+    }
 }
