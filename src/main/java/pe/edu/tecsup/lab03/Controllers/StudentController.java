bfb1a3a661e03a6138b8db27ffec8996e99e6637
Jissell <jissell.velasquez@tecsup.edu.pe>
2025-09-03 22:37:20 +0200

    feat: agregar método helloStudent en StudentController

--- src/main/java/pe/edu/tecsup/lab03/Controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/Controllers/StudentController.java
@@ -2,7 +2,4 @@ package pe.edu.tecsup.lab03.Controllers;
 
 public class StudentController {
 
-    public String helloStudent() {
-        return "Hola estudiante, el controlador funciona correctamente!";
-    }
 }
